442af7fb8668716fb4757eafc64ce2229fda62fb
Ankit Kumar <121275934+ankitx66@users.noreply.github.com>
2024-07-10 22:16:41 +0530

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,9 +1,9 @@
-import moviepy.editor
-from tkinter.filedialog import *
+import moviepy.editor # import karo moviepy.editor for video to audio ko convert karne k liye
+from tkinter.filedialog import * # gui k liye dialog box open hoga
 
-path = askopenfilename()
-select = moviepy.editor.VideoFileClip(path)
-covert = select.audio
+path = askopenfilename() # video ka path dhundo
+select = moviepy.editor.VideoFileClip(path) # video ko select karo
+covert = select.audio # .audio convert kardega
 
-convert.write_audiofile("saved.mp3")
+convert.write_audiofile("saved.mp3") # audio convert karne k liye
 print("audio converted successfully ! ")
